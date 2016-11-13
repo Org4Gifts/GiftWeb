842320d17056d2227c47236e6c3eb7a879a7850f
user <user@NB-3TOJ3JD>
2016-11-14 01:33:47 +0800

    將作業套入專案中

--- src/tw/youth/java2016/giftservlet/Main.java
+++ src/tw/youth/java2016/giftservlet/Main.java
@@ -2,19 +2,16 @@ package tw.youth.java2016.giftservlet;
 
 import java.io.IOException;
 
-import javax.servlet.RequestDispatcher;
 import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
-import com.sun.org.apache.xalan.internal.xsltc.runtime.Parameter;
-
 /**
  * Servlet implementation class Main
  */
-@WebServlet("/Main")
+@WebServlet("/Alaya")
 public class Main extends HttpServlet {
 	private static final long serialVersionUID = 1L;
 
